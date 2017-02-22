3bcc56aa61f6356b571c21c245b4772a851d65b1
ck <ck@timevale.com>
2017-02-22 20:41:45 +0800

    InputWithLabel
    修改InputWithLabel为es6有状态组件

--- src/components/InputWithLabel.js
+++ src/components/InputWithLabel.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
-const InputWithLabel = (props) => {
-  const { labelName, style, ...otherProps } = props;
-  console.log(otherProps);
-  return (
-    <div className="input-item" style={style}>
-      <div className="input-label">{labelName}</div>
-      <input className="input" {...otherProps} />
-    </div>
-  );
-};
+class InputWithLabel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    const { labelName, style, ...otherProps } = this.props;
+    return (
+      <div className="input-item" style={style}>
+        <div className="input-label">{labelName}</div>
+        <input className="input" {...otherProps} />
+      </div>
+    );
+  }
+}
 
 InputWithLabel.propTypes = {
   labelName: React.PropTypes.string,
